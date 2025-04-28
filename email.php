5d184d35d14cb6e3930b741766e8de96270cb6c8
HalexandreLord <82584042+HalexandreLord@users.noreply.github.com>
2025-04-28 09:19:33 -0300

    Create email.php

---
+++ email.php
@@ -0,0 +1,19 @@
+<!-- Arquivo enviar_email.php -->
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+    
+    $destinatario = "seuemail@exemplo.com";
+    $assunto = "Mensagem do formulário de contato";
+    $corpo = "Nome: $nome\nE-mail: $email\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    if (mail($destinatario, $assunto, $corpo, $headers)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Falha ao enviar o e-mail.";
+    }
+}
+?>
